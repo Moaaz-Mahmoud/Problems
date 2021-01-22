986ee8720e165c670ce7c2bbdbefad0c279ccd71
Moaaz-Mahmoud <61514465+Moaaz-Mahmoud@users.noreply.github.com>
2021-01-22 10:21:44 +0200

    Create TieRopes.cpp

---
+++ Codility/C++/TieRopes.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+#include <string.h>
+#include <cstring>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define NEWLINE cout << '\n'
+#define loop(n) for(int i = 0; i < n; i++)
+#define all(a) a.begin(), a.end()
+#define MULTI_TEST int t; cin >> t; while (t--) 
+#define INF (1000000000) // One billion
+const double PI = acos(-1);
+
+int solution(int K, vector<int> &A) {
+    int n = A.size();
+    int cur = 0, ans = 0;
+    for(int i = 0; i < n; i++){
+        cur += A[i];
+        if(cur >= K){
+            ans++;
+            cur = 0;
+        }
+    }
+    return ans;
+}
