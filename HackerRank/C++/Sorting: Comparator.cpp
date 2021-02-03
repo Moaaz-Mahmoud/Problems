06cb7663349c02f348f6685dca34d09dafe32380
Moaaz-Mahmoud <61514465+Moaaz-Mahmoud@users.noreply.github.com>
2021-02-03 14:35:25 +0200

    Create Sorting: Comparator.cpp

---
+++ HackerRank/C++/Sorting: Comparator.cpp
@@ -0,0 +1,12 @@
+class Checker{
+public:
+  	// complete this method
+    static int comparator(Player a, Player b)  {
+        if(a.score > b.score)
+            return 1;
+        else if(a.score == b.score)
+            return a.name < b.name ? 1 : -1;
+        else
+            return -1;
+    }
+};
