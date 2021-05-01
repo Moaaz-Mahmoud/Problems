b73a597df4a3b57c9f9ce8cae65ba4aee423dab2
Moaaz-Mahmoud <61514465+Moaaz-Mahmoud@users.noreply.github.com>
2021-05-01 21:24:00 +0200

    Create Two Strings.java

---
+++ HackerRank/Java/Two Strings.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'twoStrings' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s1
+     *  2. STRING s2
+     */
+
+    public static String twoStrings(String s1, String s2) {
+        Set<Character> S = new HashSet<>();
+        char[] s1a = s1.toCharArray(),
+               s2a = s2.toCharArray();
+        for(char c : s1a) S.add(c);
+        for(char c : s2a) 
+            if(S.contains(c)) return "YES";
+        return "NO";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = bufferedReader.readLine();
+
+            String s2 = bufferedReader.readLine();
+
+            String result = Result.twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
