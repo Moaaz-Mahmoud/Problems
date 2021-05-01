2b9a9190fa16d8bfef4011e38e257d11b7936c8d
Moaaz-Mahmoud <61514465+Moaaz-Mahmoud@users.noreply.github.com>
2021-05-01 20:36:35 +0200

    Create Sherlock and Anagrams.cpp

---
+++ HackerRank/C++/Sherlock and Anagrams.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'sherlockAndAnagrams' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int sherlockAndAnagrams(string s) {
+    map<string, long long> M;
+    //sort(s.begin(), s.end());
+    for(int l = 1; l <= s.size(); l++){
+        for(int i = 0; i+l-1 < s.size(); i++){
+            auto sub = s.substr(i, l);
+            sort(sub.begin(), sub.end());
+            M[sub]++;
+        }
+    }
+    long long ans = 0;
+    for(auto it : M){
+        ans += it.second * (it.second-1) / 2;
+    }
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = sherlockAndAnagrams(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
