64c01b076fab5ef0ff4e5a3d92bdc2b5c0ba30fd
Moaaz Mahmoud <61514465+Moaaz-Mahmoud@users.noreply.github.com>
2024-05-07 06:51:44 +0300

    Update and rename CodeLib.py to starter.py

---
+++ starter.py
@@ -0,0 +1,40 @@
+import datetime as dt
+import math as mt
+
+def read_int():
+    return int(input())
+
+
+def read():
+    return list(map(int, input().split(' ')))
+
+
+def argsort(seq):
+    return sorted(range(len(seq)), key=seq.__getitem__)
+
+
+def solve():
+    n = read_int()
+  
+  
+
+  
+  
+
+    
+
+
+  
+
+  
+  
+
+  
+
+################################################
+
+_t = 1
+_t = int(input())
+
+for _i in range(_t):
+    solve()
